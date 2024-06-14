ad3e4bb12ab7b54c6fcd9e868e07ac7ab93066ca
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-06-14 11:08:32 +0300

    Update omni_ac50sa.mk

--- omni_ac50sa.mk
+++ omni_ac50sa.mk
@@ -10,7 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from ac50sa device
 $(call inherit-product, device/archos/ac50sa/device.mk)
